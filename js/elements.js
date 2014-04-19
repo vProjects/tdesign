077690569d64936d0cc153be22eadcca06ff90cb
vasu-naman <github@vyrazu.com>
2014-04-19 18:52:07 +0530

    meanwhile good state
    vasu naman

--- js/elements.js
+++ js/elements.js
@@ -22,22 +22,6 @@
 							fontSize: 20,
 							fontFamily: 'Comic Sans'
 						});
-						
-			//second line initialization 
-			var userText1 = new fabric.Text('',{
-							left: 100,
-							top: 130,
-							fontSize: 20,
-							fontFamily: 'Comic Sans'
-						});
-			
-			//Third line initialization 
-			var userText2 = new fabric.Text('',{
-							left: 100,
-							top: 160,
-							fontSize: 20,
-							fontFamily: 'Comic Sans'
-						});
 			
 			
 			
